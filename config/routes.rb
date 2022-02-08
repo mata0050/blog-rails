309cc279856cbda4e78153bd64d236acea14ac29
mata0050 <mata0050@outlook.com>
2022-02-08 10:04:27 -0500

    blog form, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "articles#index"
+  root 'welcome#index'
 end
